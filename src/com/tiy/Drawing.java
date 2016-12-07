54e74a1542e6ebb9cef246f944febb4582daa1c4
Paul Dennis <pauldennis.misc@gmail.com>
2016-12-07 16:34:33 -0500

    Loop cleanup

--- src/com/tiy/Drawing.java
+++ src/com/tiy/Drawing.java
@@ -4,28 +4,19 @@ public class Drawing {
 
     public static void printBoard(char[][] theChar){
 
-        StringBuilder s1 = new StringBuilder("||");
-        StringBuilder s2 = new StringBuilder("||");
-        StringBuilder s3 = new StringBuilder("||");
+        StringBuilder line1 = new StringBuilder("||");
+        StringBuilder line2 = new StringBuilder("||");
+        StringBuilder line3 = new StringBuilder("||");
 
-        for(int row = 0; row < 3; row++){
-            for (int column = 0; column < 3; column++){
-                s1.insert(0, theChar[0][0]);
-                s1.insert(2, theChar[0][1]);
-                s1.insert(4, theChar[0][2]);
-                s2.insert(0, theChar[1][0]);
-                s2.insert(2, theChar[1][1]);
-                s2.insert(4, theChar[1][2]);
-                s3.insert(0, theChar[2][0]);
-                s3.insert(2, theChar[2][1]);
-                s3.insert(4, theChar[2][2]);
+        StringBuilder[] lines = {line1, line2, line3};
 
-                System.out.println(s1);
-                System.out.println(s2);
-                System.out.println(s3);
-                System.out.println();
-
+        for(int row = 0; row < 3; row++) {
+            for (int column = 0; column < 3; column++) {
+                lines[row].insert(column * 2, theChar[row][column]);
             }
+        }
+        for (StringBuilder line : lines) {
+            System.out.println(line);
         }
     }
 }
